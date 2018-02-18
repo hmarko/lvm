85df4e4b4e67fe7de82f87cec0afb07aa2eff2af
Haim Marko <haim.marko@gmail.com>
2018-02-18 04:38:39 +0000

    devel

--- manage_pvmove.pl
+++ manage_pvmove.pl
@@ -2,48 +2,74 @@
 
 use JSON;
 
-@file = `cat pvmove.txt`;
-foreach my $line (@file) {
-	chomp $line;
-	($srcpv,$dstpv) = split(/\s+/,$line);
-	$pvsrc{$srcpv}{name}=$dstpv;
-	$pvdst{$dstpv}{name}=$srcpv;
-	if (exists $pvsrc{$dstpv} or exists $pvdst{$srcpv}) {
-		print "error in config file for dst:$dstpv or src:$srcpv\n";
-		exit 1;
-	}
-}
-@diskscan = `lvmdiskscan`;
-foreach my $line (@diskscan) {
-	chomp $line;
-	if ($line=~/(\/dev\/mapper\/\S+).+\[\s+([0-9]*\.[0-9]+|[0-9]+)\s+(\S+)\]\s+(LVM physical volume)/) {
-		if (exists $pvsrc{$1}) {
-			$pvsrc{$1}{size} = $2;
-			$pvsrc{$1}{quantifier} = $3;
-			$pvsrc{$1}{pvconfigured} = 1;
+$deviceprefix = '/dev/mapper/';
+$pvcreateparams = '--dataalignment 4k';
+sub createmapping {
+	$pv = false;
+	@file = `cat pvmove.txt`;
+	foreach my $line (@file) {
+		chomp $line;
+		($srcpv,$dstpv) = split(/\s+/,$line);
+		$pv{$srcpv}{destname}=$dstpv;
+		$pv{$dstpv}{srcname}=$srcpv;
+		if (exists $pvsrc{$dstpv} or exists $pvdst{$srcpv}) {
+			print "error in config file for dst:$dstpv or src:$srcpv\n";
+			exit 1;
 		}
-                if (exists $pvdst{$1}) {
-                        $pvdst{$1}{size} = $2;
-                        $pvdst{$1}{quantifier} = $3;
-                        $pvdst{$1}{pvconfigured} = 1;
-                }
 	}
-        if ($line=~/(\/dev\/mapper\/\S+).+\[\s+([0-9]*\.[0-9]+|[0-9]+)\s+(\S+)\]\s+$/) {
-                if (exists $pvsrc{$1}) {
-                        $pvsrc{$1}{size} = $2;
-                        $pvsrc{$1}{quantifier} = $3;
-                        $pvsrc{$1}{pvconfigured} = 0;
-                }
-                if (exists $pvdst{$1}) {
-                        $pvdst{$1}{size} = $2;
-                        $pvdst{$1}{quantifier} = $3;
-                        $pvdst{$1}{pvconfigured} = 0;
-                }
-        }
-	
+	@diskscan = `lvmdiskscan`;
+	foreach my $line (@diskscan) {
+		chomp $line;
+		if ($line=~/$deviceprefix(\S+).+\[\s+([0-9]*\.[0-9]+|[0-9]+)\s+(\S+)\]\s+(LVM physical volume)/) {
+			$pv{$1}{size} = $2;
+			$pv{$1}{quantifier} = $3;
+			$pv{$1}{configured} = 1;
+		}
+		if ($line=~/$deviceprefix(\S+).+\[\s+([0-9]*\.[0-9]+|[0-9]+)\s+(\S+)\]\s+$/) {
+			$pv{$1}{size} = $2;
+			$pv{$1}{quantifier} = $3;
+			$pv{$1}{configured} = 0;	
+		}
+	}
+
+	@diskscan = `pvs --units m`;
+	foreach my $line (@diskscan) {
+		chomp $line;
+		if ($line=~/$deviceprefix(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+([0-9]*\.[0-9]+|[0-9]+)(\w+)\s+([0-9]*\.[0-9]+|[0-9]+)(\w+)/) {
+			$pv{$1}{vg} = $2;
+			$pv{$1}{vgsize} = $5;
+			$pv{$1}{vgsizequantifer} = $6;
+			$pv{$1}{vgfree} = $7;
+			$pv{$1}{vgfreequatifier} = $8;
+		}
+	}
 }
 
-my $pvdstjson = encode_json \%pvdst;
-my $pvsrcjson = encode_json \%pvsrc;
+createmapping();
 
-print " $pvsrcjson\n";
+foreach $p (keys %pv) {
+	if (not $pv{$p}{configured} and $pv{$p}{srcname}) {
+		$dev = $deviceprefix.$p;
+		print "creating PV: $dev :";
+		@pvcreate = `pvcreate $pvcreateparams $dev`;
+		print "$pvcreate[0]\n";
+	}
+}
+createmapping();
+
+foreach $p (keys %pv) {
+	if ($pv{$p}{configured} and $pv{$p}{srcname} and not $pv{$p}{vg}) {
+		$vg = $pv{$pv{$p}{srcname}}{vg};
+		if ($vg) {
+			$dev = $deviceprefix.$p;
+			print "extanding VG:$vg with $dev :";
+			@pvextand = `vgextend $vg $dev`;
+			print "$pvextand[0]\n";
+		}
+	}
+}
+createmapping();
+
+my $pvjson = encode_json \%pv;
+
+print " $pvjson\n";
