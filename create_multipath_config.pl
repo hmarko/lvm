cbab0d911c7c65f2bed4c2fdaab6f22f0efa6ba0
Haim Marko <haim.marko@gmail.com>
2018-02-25 18:46:39 +0200

    initial

--- create_multipath_config.pl
+++ create_multipath_config.pl
@@ -7,9 +7,11 @@ $server = $ARGV[0];
 $svm = $ARGV[1];
 $app = $ARGV[2];
 $aggr = $ARGV[3];
+$igroup = $ARGV[4];
 
-$vgs = $ARGV[4];
+$vgs = $ARGV[5];
 
+$wwidprefix = '3600a0980';
 
 $vol{'size'} = '100';
 $vol{'max-autosize'} = '15t';
@@ -52,9 +54,13 @@ foreach $inputvg (split(/,/,$vgs)) {
 		if ($line =~ /\/dev\/mapper\/(.\S+)\s+(\S+)\s+([0-9]*\.[0-9]+|[0-9]+)G/) {
 			
 			$dev = $1;
+			if ($dev =~/^cdotsan_/) {
+				print "ERROR: VG:$inputvg already contains NetApp PV:$dev\n";
+				exit;
+			}
 			$vg = $2;
 			#add 1g on the netapp luns
-			$sizeg = $3 + 1;
+			$sizeg = floor($3) + 1;
 			if ($vg eq $inputvg) {
 				$vol{'total-pv-size'} += $sizeg;
 				$vol{'vgs'}{$vg}{'lun-count'} += 1;
@@ -76,7 +82,6 @@ if ($vol{'total-pv-size'} * 2 > 100) {
 }
 
 $cmd = "volume show -volume $volume -field state,type";
-print "$cmd\n";
 @out = `$sshcmdsvm $cmd`;
 
 if ($out[2]=~/$svm\s+$volume\s+(\S+)\s+(\S+)/) {
@@ -96,15 +101,100 @@ if ($out[2]=~/$svm\s+$volume\s+(\S+)\s+(\S+)/) {
 @out = `$sshcmdsvm $cmd`;
 print @out;
 
-@luns = `$sshcmdsvm lun show -fields path,size,state,mapped -volume $volume`;
-@lunmappingss = `$sshcmdsvm lun mapping show -volume $volume`;
+@existingluns = `$sshcmdsvm \"set -units gb;lun show -fields path,size,state,mapped -volume $volume\"`;
+@existinglunmappingss = `$sshcmdsvm lun mapping show -volume $volume -fields path,igroup`;
+
+@igroup = `$sshcmdsvm igroup show $igroup -fields initiator`;
+
+if (not $igroup[2] =~ /\s+($igroup)\s+/) {
+	print "ERROR: igroup:$igroup does not exists\n";
+	exit 1;
+}
 
 foreach $vg (keys %{$vol{'vgs'}}) {
 	for ($lun=1;$lun <= $vol{'vgs'}{$vg}{'lun-count'};$lun++) {
 		$lunpath = '/vol/'.$volume.'/'.$vg.'_'.$lun;
 		$lunsize = $vol{'vgs'}{$vg}{'luns'}[$lun];
+		
+		print "creating lun: $lunpath\n";
+		$found = 0;
+		foreach $lun (@existingluns ) {
+			if ($lun =~/^(\S+)\s+($lunpath)\s+(\d+)GB\s+(\S+)/) {
+				$size = $3.'GB';
+				$state = $4;
+				print "WARNING: lun $lunpath already exists on this volume with size:$size state:$state\n";
+				$found = 1;
+			}
+		}
+		if (not $found) {
+			$cmd = "lun create -path $lunpath -size $lunsize".'GB -ostype linux -space-reserve disable -space-allocation enabled';
+			`$sshcmdsvm $cmd`;
+		}
+
+		$cmd = "lun show -path $lunpath -fields serial-hex";
+		@luninfo = `$sshcmdsvm $cmd`;
+		if ($luninfo[2] =~/$lunpath\s+(\S+)/) {
+			$serial = $1;
+			$vol{'vgs'}{$vg}{'created-luns'}{$lunpath} = $serial;
+		}		
+
+		foreach $lun (@existinglunmappingss ) { 
+			if ($lun=~/\S+\s+($lunpath)\s+(\S+)/) {
+				$mappedigroup = $2;
+			}
+				
+			print "mapping lun: $lunpath to igroup:$igroup\n";
+			$found = 0;		
 	}
 }
+
+
+$redhat = `$sshcmdserver cat /etc/redhat-release`;
+
+$multipathconf = `$sshcmdserver cat /etc/multipath.conf`;
+
+$addvendor = 1;
+if ($multipathconf =~ /^\s*vendor\s+ \"NETAPP\"/) {
+	$addvendor = 0;
+}
+
+$newfile = '';
+foreach $line (split(/\n/,$multipathconf)) {
+	$newfile .= "$line\n";
+	
+	chomp $line;
+	if ($line =~ /^\s*devices\s*\{\s*$/ and $addvendor) {
+		$addvendor = 0;
+		$newfile .= << 'END_TEXT'
+	device {
+		vendor "NETAPP"
+		product "LUN"
+		path_grouping_policy group_by_prio
+		features "3 queue_if_no_path pg_init_retries 50"
+		prio_callout "/sbin/mpath_prio_alua /dev/%n"
+		path_checker tur
+		path_selector "round-robin 0"
+		failback immediate
+		hardware_handler "1 alua"
+		rr_weight uniform
+		rr_min_io 128
+		getuid_callout "/sbin/scsi_id -g -u -s /block/%n"
+	}
+END_TEXT
+	}
+	if ($line =~ /^\s*multipaths\s*\{\s*$/) {
+		
+		foreach $vg (keys %{$vol{'vgs'}}) {
+			foreach $lunpath (keys %{$vol{'vgs'}{$vg}{'created-luns'}}) {
+				$serial = $vol{'vgs'}{$vg}{'created-luns'}{$lunpath };
+				print "$lunpath  $serial\n";
+			}
+		}
+		
+	}
+	
+}
+
 
 exit ;
 
